042eafafed4deaf623ae87cfbcc226fc8871927a
Matthew Flatt <mflatt@racket-lang.org>
2013-05-24 21:57:31 -0600

    reorganize into core plus packages
    The "racket" directory contains a pared-back version of the
    repository, roughly.
    
    The "pkgs" directory everything else in the repository, but
    organized into packages.
    
    original commit: b2ebb0a28bf8136e75cd98316c22fe54c30eacb2

--- pkgs/gui-pkgs/gui-doc/scribblings/gui/common.rkt
+++ pkgs/gui-pkgs/gui-doc/scribblings/gui/common.rkt
@@ -4,13 +4,13 @@
            racket/class
            racket/contract
            "blurbs.rkt"
-           (only-in "../reference/mz.rkt" AllUnix exnraise))
+           (only-in scribblings/reference/mz AllUnix exnraise))
   (provide (all-from-out scribble/manual)
            (all-from-out scribble/basic)
            (all-from-out racket/class)
            (all-from-out racket/contract)
            (all-from-out "blurbs.rkt")
-           (all-from-out "../reference/mz.rkt"))
+           (all-from-out scribblings/reference/mz))
 
   (require (for-label racket/gui/base
                       racket/class
