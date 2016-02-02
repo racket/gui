a2f4b59ea8b6466a7c1e41ac37c12ae331659aa7
Ryan Culpepper <ryanc@racket-lang.org>
2016-02-01 19:17:14 -0500

    split out mrlib/arrow-toggle-snip

--- gui-lib/mrlib/syntax-browser.rkt
+++ gui-lib/mrlib/syntax-browser.rkt
@@ -15,7 +15,8 @@ needed to really make this work:
            racket/match
            racket/contract
            (prefix-in : racket/base)
-           "include-bitmap.rkt")
+           "include-bitmap.rkt"
+           "arrow-toggle-snip.rkt")
 
   (define orig-output-port (current-output-port))
   (define (oprintf . args) (apply fprintf orig-output-port args))
@@ -336,7 +337,7 @@ needed to really make this work:
       
       (let ()
         
-        (send outer-t insert (new turn-snip% 
+        (send outer-t insert (new arrow-toggle-snip% 
                                   [on-up (λ () (hide-details))]
                                   [on-down (λ () (show-details))]))
         (send outer-t insert (format "~s\n" main-stx))
@@ -488,118 +489,6 @@ needed to really make this work:
   (void (send green-style-delta set-delta-foreground "forest green"))
   (define small-style (make-object style-delta% 'change-size 4))
   
-  (define turn-snip%
-    (class snip%
-      
-      (init-field on-up on-down)
-      
-      ;; state : (union 'up 'down 'up-click 'down-click))
-      (init-field [state 'up])
-      
-      (define/override (copy)
-        (instantiate turn-snip% ()
-          (on-up on-up)
-          (on-down on-down)
-          (state state)))
-      
-      (define/override (draw dc x y left top right bottom dx dy draw-caret)
-        (let ([bitmap (case state
-                        [(up) up-bitmap]
-                        [(down) down-bitmap]
-                        [(up-click) up-click-bitmap]
-                        [(down-click) down-click-bitmap])])
-          (cond
-            [(send bitmap ok?)
-             (send dc draw-bitmap bitmap x y)]
-            [(send dc draw-rectangle x y 10 10)
-             (send dc drawline x y 10 10)])))
-             
-
-      (define/override (get-extent dc x y w h descent space lspace rspace)
-        (set-box/f! descent 0)
-        (set-box/f! space 0)
-        (set-box/f! lspace 0)
-        (set-box/f! rspace 0)
-        (set-box/f! w arrow-snip-width)
-        (set-box/f! h arrow-snip-height))
-      
-      (define/override (on-event dc x y editorx editory evt)
-        (let ([snip-evt-x (- (send evt get-x) x)]
-              [snip-evt-y (- (send evt get-y) y)])
-          (cond
-            [(send evt button-down? 'left)
-             (set-state (case state
-                          [(up) 'up-click]
-                          [(down) 'down-click]
-                          [else 'down-click]))]
-            [(and (send evt button-up? 'left)
-                  (<= 0 snip-evt-x arrow-snip-width)
-                  (<= 0 snip-evt-y arrow-snip-height))
-             (set-state (case state
-                          [(up up-click) 
-                           (on-down)
-                           'down]
-                          [(down down-click)
-                           (on-up)
-                           'up]
-                          [else 'down]))]
-            [(send evt button-up? 'left)
-             (set-state (case state
-                          [(up up-click) 'up]
-                          [(down down-click) 'down]
-                          [else 'up]))]
-            [(and (send evt get-left-down)
-                  (send evt dragging?)
-                  (<= 0 snip-evt-x arrow-snip-width)
-                  (<= 0 snip-evt-y arrow-snip-height))
-             (set-state (case state
-                          [(up up-click) 'up-click]
-                          [(down down-click) 'down-click]
-                          [else 'up-click]))]
-            [(and (send evt get-left-down)
-                  (send evt dragging?))
-             (set-state (case state
-                          [(up up-click) 'up]
-                          [(down down-click) 'down]
-                          [else 'up-click]))]
-            [else
-             (super on-event dc x y editorx editory evt)])))
-
-      (inherit get-admin)
-      (define/private (set-state new-state)
-        (unless (eq? state new-state)
-          (set! state new-state)
-          (let ([admin (get-admin)])
-            (when admin
-              (send admin needs-update this 0 0 arrow-snip-width arrow-snip-height)))))
-      
-      (define/override (adjust-cursor dc x y editorx editory event) arrow-snip-cursor)
-      
-      (super-instantiate ())
-      
-      (inherit get-flags set-flags)
-      (set-flags (cons 'handles-events (get-flags)))))
-  
-  (define (set-box/f! b v) (when (box? b) (set-box! b v)))
-  
-  (define down-bitmap (include-bitmap (lib "icons/turn-down.png") 'png))
-  (define up-bitmap (include-bitmap (lib "icons/turn-up.png") 'png))
-  (define down-click-bitmap (include-bitmap (lib "icons/turn-down-click.png") 'png))
-  (define up-click-bitmap (include-bitmap (lib "icons/turn-up-click.png") 'png))
-  (define arrow-snip-height
-    (max 10
-         (send up-bitmap get-height)
-         (send down-bitmap get-height)
-         (send up-click-bitmap get-height)
-         (send down-click-bitmap get-height)))
-  (define arrow-snip-width
-    (max 10
-         (send up-bitmap get-width)
-         (send down-bitmap get-width)
-         (send up-click-bitmap get-width)
-         (send down-click-bitmap get-width)))
-  (define arrow-snip-cursor (make-object cursor% 'arrow))
-
   ;; make-text-port : text -> port
   ;; builds a port from a text object.  
   (define (make-text-port text)
