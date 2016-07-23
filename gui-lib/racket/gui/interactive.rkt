cb81e3768df25aa72b6fcf36f8aaf326e26966db
Leif Andersen <leif@leifandersen.net>
2016-07-23 02:27:40 -0400

    Woops, racket/gui/interactive should load .gracketrc.
    (Cannot use (find-system-path 'init-file) because it will always
    evaluate to .racketrc, even in gracket (where it should be
    .gracketrc).)

--- gui-lib/racket/gui/interactive.rkt
+++ gui-lib/racket/gui/interactive.rkt
@@ -1,1 +1,12 @@
 #lang racket/gui
+
+(let ([init-file (cleanse-path
+                  (build-path
+                   (find-system-path 'init-dir)
+                   ; Cludge because (find-system-path init-path) will
+                   ;   always return .racketrc, even in gracket
+                   (case (system-type)
+                     [(unix macosx) ".gracketrc"]
+                     [(windows) "gracketrc.rktl"])))])
+  (when (file-exists? init-file)
+    (load init-file)))
