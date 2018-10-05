534f7409c25969b555e1d536df224954b456eaa4
Matthew Flatt <mflatt@racket-lang.org>
2018-10-05 16:12:21 -0600

    Revert "further display correction for Mojave"
    This reverts commit 7be57985f827e4fd739620835bdc6b2823199160.

--- gui-lib/mred/private/wx/cocoa/queue.rkt
+++ gui-lib/mred/private/wx/cocoa/queue.rkt
@@ -27,7 +27,6 @@
               post-dummy-event
 
               try-to-sync-refresh
-              try-to-flush
               sync-cocoa-events
               set-screen-changed-callback!)
 
@@ -519,13 +518,6 @@
   ;; atomically => outside of the event loop
   (atomically
    (pre-event-sync #t)))
-
-(define (try-to-flush)
-  (tell app nextEventMatchingMask: #:type _NSUInteger 0
-        untilDate: #f
-        inMode: NSDefaultRunLoopMode
-        dequeue: #:type _BOOL #t)
-  (void))
 
 (set-platform-queue-sync!
  (lambda ()
