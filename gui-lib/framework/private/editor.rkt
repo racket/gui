racket/gui

commit a71ab260f29b78fecb862c0f25764375d9912450

adjust the can-save-file? callbacks to be sensitive to when autosaving is happening and, in particular, to never ask the user a question in that case, but instead pick a safe default behavior
related to racket/drracket#391

gui-lib/framework/private/editor.rkt
@@ -147,16 +147,25 @@
                get-admin)
       
       (define/augment (can-save-file? filename format)
+        (define (ask-users-opinion)
+          (cond
+            [(doing-autosave?)
+             ;; opt for the effect of
+             ;; clicking on the `cancel`
+             ;; button
+             #f]
+            [else
+             (gui-utils:get-choice
+              (string-constant file-has-been-modified)
+              (string-constant overwrite-file-button-label)
+              (string-constant cancel)
+              (string-constant warning)
+              #f
+              (get-top-level-window)
+              #:dialog-mixin frame:focus-table-mixin)]))
         (and (if (equal? filename (get-filename))
                  (if (save-file-out-of-date?)
-                     (gui-utils:get-choice 
-                      (string-constant file-has-been-modified)
-                      (string-constant overwrite-file-button-label)
-                      (string-constant cancel)
-                      (string-constant warning)
-                      #f
-                      (get-top-level-window)
-                      #:dialog-mixin frame:focus-table-mixin)
+                     (ask-users-opinion)
                      #t)
                  #t)
              (inner #t can-save-file? filename format)))

gui-lib/framework/private/text-misc.rkt
@@ -198,19 +198,29 @@
           (and (not (all-string-snips))
                (eq? format 'same)
                (eq? 'text (get-file-format))))
+        (define (get-users-opinion)
+          (cond
+            [(editor:doing-autosave?)
+             ;; we don't ask for the user's opinion here;
+             ;; we opt for clicking the `dont-save` button
+             ;; when the file is explicitly saved, we'll
+             ;; do the actual asking
+             3]
+            [else
+             (message-box/custom
+              (string-constant warning)
+              (string-constant save-as-binary-format)
+              (string-constant convert-format)
+              (string-constant keep-format)
+              (string-constant dont-save)
+              #f
+              '(disallow-close default=3)
+              3
+              #:dialog-mixin frame:focus-table-mixin)]))
         (define format-converted
           (and needs-wxme?
                (or (not (preferences:get 'framework:verify-change-format))
-                   (message-box/custom
-                    (string-constant warning)
-                    (string-constant save-as-binary-format)
-                    (string-constant convert-format)
-                    (string-constant keep-format)
-                    (string-constant dont-save)
-                    #f
-                    '(disallow-close default=3)
-                    3
-                    #:dialog-mixin frame:focus-table-mixin))))
+                   (get-users-opinion))))
         (define continue-saving?
           (case format-converted
             [(1 #t)

commit 6ccf3dcb6db1dd9ce2fa3834946064575e416948

fixed bug introduced in a71ab260f29b78fecb862c0f25764375d9912450
when automatically saving due to, say, tab switching,
the internal date wasn't being updated properly so
there could be spurious warnings about overwriting files

gui-lib/framework/private/editor.rkt
@@ -163,13 +163,17 @@
               #f
               (get-top-level-window)
               #:dialog-mixin frame:focus-table-mixin)]))
-        (and (if (equal? filename (get-filename))
-                 (if (save-file-out-of-date?)
-                     (ask-users-opinion)
-                     #t)
-                 #t)
-             (inner #t can-save-file? filename format)))
-      
+        (define result
+          (and (if (equal? filename (get-filename))
+                   (if (save-file-out-of-date?)
+                       (ask-users-opinion)
+                       #t)
+                   #t)
+               (inner #t can-save-file? filename format)))
+        (when result (set! can-save-file-filename filename))
+        result)
+
+      (define can-save-file-filename #f)
       (define last-saved-file-time #f)
       
       (define/augment (after-save-file success?)
@@ -180,16 +184,19 @@
         (unless (unbox temp-b)
           (when filename
             (handler:add-to-recent filename)))
-        
-        ;; update last-saved-file-time
-        (unless (doing-autosave?)
+
+        ;; if the filenames are different, then the save
+        ;; was an auto-save to a temporary file so we
+        ;; don't want to update the last-saved-file-time
+        (when (equal? filename can-save-file-filename)
           (unless (unbox temp-b)
             (when success?
               (set! last-saved-file-time
                     (and filename
                          (file-exists? filename)
                          (file-or-directory-modify-seconds filename))))))
-        
+
+        (set! can-save-file-filename #f)
         (inner (void) after-save-file success?))
       
       (define/augment (after-load-file success?)