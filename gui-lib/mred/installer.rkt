16147c5e5ac8a7b0064a2a23e64d5b3d7c2013a9
Matthew Flatt <mflatt@racket-lang.org>
2021-05-19 11:40:02 -0600

    adapt GRacket/MrEd installers for layered and tethered installations
    Don't repeat executables for non-tethered layer or when tethering.

--- gui-lib/mred/installer.rkt
+++ gui-lib/mred/installer.rkt
@@ -1,10 +1,9 @@
 #lang racket/base
 (require launcher 
          compiler/embed
-         racket/file
-         racket/path
          setup/dirs
-         setup/cross-system)
+         setup/cross-system
+         (submod racket/gui/installer private-install-helpers))
 
 (provide installer)
 
@@ -12,64 +11,60 @@
 (define mred-exe-systems '(unix))
 
 (define (installer path coll user? no-main?)
-  (unless no-main?
-    (do-installer path coll user? #f)
-    (when (and (not user?)
-               (find-config-tethered-console-bin-dir))
-      (do-installer path coll #f #t)))
-  (when (find-addon-tethered-console-bin-dir)
-    (do-installer path coll #t #t)))
+  (dispatch-to-installer-maker path coll user? no-main? do-installer))
 
 (define (do-installer path coll user? tethered?)
   (define variants (available-mred-variants))
   (when (memq (cross-system-type) mred-exe-systems)
     (for ([v variants] #:when (memq v '(3m cgc cs)))
       (parameterize ([current-launcher-variant v])
-        (create-embedding-executable
-         (prep-dir (mred-program-launcher-path "MrEd"
-                                               #:user? user?
-                                               #:tethered? tethered?))
-         #:cmdline (append
-                    (if tethered? (if user? (addon-flags) (config-flags)) null)
-                    '("-I" "scheme/gui/init"))
-         #:variant v
-         #:launcher? #t
-         #:gracket? #t
-         #:aux `((relative? . ,(not user?)))))))
+        (define exe-name (mred-program-launcher-path "MrEd"
+                                                     #:user? user?
+                                                     #:tethered? tethered?))
+        (unless (exists-in-another-layer? exe-name user? tethered? #:gui? #t)
+          (create-embedding-executable
+           (prep-dir exe-name)
+           #:cmdline (append
+                      ;; doing the same thing as a `make-gracket-launcher` #:tether-mode:
+                      (if tethered? (if user? (addon-flags) (config-flags)) null)
+                      '("-I" "scheme/gui/init"))
+           #:variant v
+           #:launcher? #t
+           #:gracket? #t
+           #:aux `((relative? . ,(not user?))))))))
   ;; add a mred-text executable that uses the -z flag (preferring a script)
   (define tether-mode (and tethered? (if user? 'addon 'config)))
   (for ([vs '((script-3m 3m) (script-cgc cgc) (script-cs cs))])
     (let ([v (findf (lambda (v) (memq v variants)) vs)])
       (when v
         (parameterize ([current-launcher-variant v])
-          (make-gracket-launcher
-           #:tether-mode tether-mode
-           '("-I" "scheme/gui/init" "-z")
-           (prep-dir (mred-program-launcher-path "mred-text"
-                                                 #:user? user?
-                                                 #:tethered? tethered?
-                                                 #:console? #t))
-           `([relative? . ,(not (or user? tethered?))]
-             [subsystem . console]
-             [single-instance? . #f]))))))
+          (define exe-name (mred-program-launcher-path "mred-text"
+                                                       #:user? user?
+                                                       #:tethered? tethered?
+                                                       #:console? #t))
+          (unless (exists-in-another-layer? exe-name user? tethered? #:gui? #f)
+            (make-gracket-launcher
+             #:tether-mode tether-mode
+             '("-I" "scheme/gui/init" "-z")
+             (prep-dir exe-name)
+             `([relative? . ,(not (or user? tethered?))]
+               [subsystem . console]
+               [single-instance? . #f])))))))
   ;; add bin/mred script under OS X
   (when (eq? 'macosx (cross-system-type))
     (for ([v variants] #:when (memq v '(script-3m script-cgc script-cs)))
       (parameterize ([current-launcher-variant v])
-        (make-gracket-launcher
-         #:tether-mode tether-mode
-         null
-         (prep-dir (mred-program-launcher-path "MrEd"
-                                               #:user? user?
-                                               #:tethered? tethered?))
-         `([exe-name . "GRacket"]
-           [relative? . ,(not (or user? tethered?))]
-           [exe-is-gracket . #t]))))))
-
-(define (prep-dir p)
-  (define dir (path-only p))
-  (make-directory* dir)
-  p)
+        (define exe-name (mred-program-launcher-path "MrEd"
+                                                     #:user? user?
+                                                     #:tethered? tethered?))
+        (unless (exists-in-another-layer? exe-name user? tethered? #:gui? #f)
+          (make-gracket-launcher
+           #:tether-mode tether-mode
+           null
+           (prep-dir exe-name)
+           `([exe-name . "GRacket"]
+             [relative? . ,(not (or user? tethered?))]
+             [exe-is-gracket . #t])))))))
 
 (define (addon-flags)
   (append
--- gui-lib/racket/gui/installer.rkt
+++ gui-lib/racket/gui/installer.rkt
@@ -1,19 +1,49 @@
 #lang racket/base
-(require launcher
-         racket/path
-         racket/file
-         setup/dirs)
+(require launcher)
 
 (provide installer)
 
+(module private-install-helpers racket/base
+  (require setup/dirs
+           racket/file
+           racket/path)
+
+  (provide dispatch-to-installer-maker
+           exists-in-another-layer?
+           prep-dir)
+
+  (define (dispatch-to-installer-maker path coll user? no-main? do-installer)
+    (cond
+      [user?
+       (if (find-addon-tethered-console-bin-dir)
+           (do-installer path coll #t #t)
+           (do-installer path coll #t #f))]
+      [else
+       (unless no-main?
+         (if (find-config-tethered-console-bin-dir)
+             (do-installer path coll #f #t)
+             (do-installer path coll #f #f)))]))
+
+  (define (exists-in-another-layer? exe-name user? tethered? #:gui? gui?)
+    ;; for an untethered main installation, check whether the
+    ;; executable exists already in an earlier layer
+    (and (not user?)
+         (not tethered?)
+         (let-values ([(base name dir?) (split-path exe-name)])
+           (for/or ([dir (in-list (if gui?
+                                      (get-gui-bin-extra-search-dirs)
+                                      (get-console-bin-extra-search-dirs)))])
+             (file-or-directory-type (build-path dir name) #f)))))
+
+  (define (prep-dir p)
+    (define dir (path-only p))
+    (make-directory* dir)
+    p))
+
+(require (submod "." private-install-helpers))
+
 (define (installer path coll user? no-main?)
-  (unless no-main?
-    (do-installer path coll user? #f)
-    (when (and (not user?)
-               (find-config-tethered-console-bin-dir))
-      (do-installer path coll #f #t)))
-  (when (find-addon-tethered-console-bin-dir)
-    (do-installer path coll #t #t)))
+  (dispatch-to-installer-maker path coll user? no-main? do-installer))
 
 (define (do-installer path collection user? tethered?)
   (define variants (available-mred-variants))
@@ -23,31 +53,28 @@
     (let ([v (findf (lambda (v) (memq v variants)) vs)])
       (when v
         (parameterize ([current-launcher-variant v])
-          (make-mred-launcher
-           #:tether-mode tether-mode
-           '("-z")
-           (prep-dir
-            (mred-program-launcher-path "gracket-text"
-                                        #:user? user?
-                                        #:tethered? tethered?
-                                        #:console? #t))
-           `([subsystem . console]
-             [single-instance? . #f]
-             [relative? . ,(not (or user? tethered?))]))))))
+          (define exe-name (mred-program-launcher-path "gracket-text"
+                                                       #:user? user?
+                                                       #:tethered? tethered?
+                                                       #:console? #t))
+          (unless (exists-in-another-layer? exe-name user? tethered? #:gui? #f)
+            (make-mred-launcher
+             #:tether-mode tether-mode
+             '("-z")
+             (prep-dir exe-name)
+             `([subsystem . console]
+               [single-instance? . #f]
+               [relative? . ,(not (or user? tethered?))])))))))
   ;; add a bin/gracket (in addition to lib/gracket)
   (for ([vs '((script-3m 3m) (script-cgc cgc) (script-cs cs))])
     (let ([v (findf (lambda (v) (memq v variants)) vs)])
       (when v
         (parameterize ([current-launcher-variant v])
-          (make-mred-launcher #:tether-mode tether-mode
-                              null
-                              (prep-dir
-                               (mred-program-launcher-path "GRacket" #:user? user? #:tethered? tethered?))
-                              `([exe-name . "GRacket"]
-                                [relative? . ,(not (or user? tethered?))]
-                                [exe-is-gracket . #t])))))))
-
-(define (prep-dir p)
-  (define dir (path-only p))
-  (make-directory* dir)
-  p)
+          (define exe-name (mred-program-launcher-path "GRacket" #:user? user? #:tethered? tethered?))
+          (unless (exists-in-another-layer? exe-name user? tethered? #:gui? #f)
+            (make-mred-launcher #:tether-mode tether-mode
+                                null
+                                (prep-dir exe-name)
+                                `([exe-name . "GRacket"]
+                                  [relative? . ,(not (or user? tethered?))]
+                                  [exe-is-gracket . #t]))))))))
