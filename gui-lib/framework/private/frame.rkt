racket/gui

commit 3d5806c3a264b879b02c58adca31efcef907bd81

another attempt to make the search bar have the correct size more often

gui-lib/framework/private/frame.rkt
@@ -2429,7 +2429,11 @@
                [root (make-object % s-root)])
           (set! super-root s-root)
           root)))
-    
+
+    (define/override (on-activate on?)
+      (when on?
+        (try-to-update-canvas-in-case-font-size-changed)))
+
     (define text-to-search #f)
     (define/public-final (get-text-to-search) text-to-search)
 
@@ -2500,12 +2504,15 @@
         (when focus?
           (send find-edit set-position 0 (send find-edit last-position))
           (send (send find-edit get-canvas) focus))
-        (let ([c (send find-edit get-canvas)])
-          (when (and c (send c get-line-count))
-            ;; try to update the canvas so that the font size is correctly accounted for
-            (send c set-editor (send c get-editor))))
+        (try-to-update-canvas-in-case-font-size-changed)
         (send find-edit end-edit-sequence)))
-    
+
+    (define/private (try-to-update-canvas-in-case-font-size-changed)
+      (when find-edit
+        (define c (send find-edit get-canvas))
+        (when (and c (send c get-line-count))
+          (send c set-editor (send c get-editor)))))
+
     (define/public (unhide-search-and-toggle-focus #:new-search-string-from-selection? [new-search-string-from-selection? #f])
       (if hidden?
         (unhide-search #t #:new-search-string-from-selection? new-search-string-from-selection?)

commit 2f552b6ce9910d425261b1d18f9201f0acf9daf9

fix bug introduced in 3d5806c3a

gui-lib/framework/private/frame.rkt
@@ -2431,6 +2431,7 @@
           root)))
 
     (define/override (on-activate on?)
+      (super on-activate on?)
       (when on?
         (try-to-update-canvas-in-case-font-size-changed)))
 